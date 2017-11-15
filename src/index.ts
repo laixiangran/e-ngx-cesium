f79f80c01e4ea6552d48499d7591e07c941a4637
laixiangran <laixiangran@163.com>
2017-11-15 23:31:10 +0800

    修复引入cesium-typings引起的bug

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@
  * homepage：http://www.laixiangran.cn
  */
 
+/// <reference path="../src/types/index.d.ts" />
+
 export { NgxCesiumModule } from './ngx-cesium.module';
 export { NgxCesiumComponent } from './ngx-cesium.component';
