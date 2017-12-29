d31b497d9aafb6321540a5acaa9d568ebad2f958
laixiangran <laixiangran@163.com>
2017-12-29 13:32:34 +0800

    新增高德地图和天地图的支持

---
+++ example/app/pages/frame/pages/onlineMap/pages/gaode/gaode.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import ViewerOptions = Cesium.ViewerOptions;
+import Viewer = Cesium.Viewer;
+import Scene = Cesium.Scene;
+import Globe = Cesium.Globe;
+import { GaodeImageryProvider, GaodeMapsStyle } from '../../../../../../../../src';
+import { SelectItem } from 'primeng/primeng';
+
+@Component({
+	selector: 'app-gaode',
+	templateUrl: './gaode.component.html',
+	styleUrls: ['./gaode.component.scss']
+})
+export class GaodeComponent implements OnInit {
+	viewerOptions: ViewerOptions;
+	viewer: Viewer;
+	scene: Scene;
+	globe: Globe;
+	servers: SelectItem[];
+	selectedServer: GaodeImageryProvider;
+
+	constructor() {
+		this.viewerOptions = {
+			imageryProvider: new GaodeImageryProvider(GaodeMapsStyle.IMG)
+		};
+		this.servers = [
+			{label: '--选择服务类型--', value: null},
+			{label: '矢量地图服务', value: new GaodeImageryProvider(GaodeMapsStyle.VEC)},
+			{label: '影像地图服务', value: new GaodeImageryProvider(GaodeMapsStyle.IMG)},
+			{label: '影像标注服务', value: new GaodeImageryProvider(GaodeMapsStyle.CIA)}
+		];
+	}
+
+	ngOnInit() {
+	}
+
+	onViewerReady(evt: any) {
+		this.viewer = evt.viewer;
+		this.scene = evt.scene;
+		this.globe = evt.globe;
+	}
+
+	serverChange($event) {
+		this.viewer.imageryLayers.removeAll();
+		let gaodeImageryProvider: GaodeImageryProvider;
+		if ($event.value) {
+			gaodeImageryProvider = $event.value;
+		} else {
+			gaodeImageryProvider = new GaodeImageryProvider(GaodeMapsStyle.IMG);
+		}
+		this.viewer.imageryLayers.addImageryProvider(gaodeImageryProvider);
+	}
+}
--- example/app/pages/frame/pages/onlineMap/pages/tdt/tdt.component.ts
+++ example/app/pages/frame/pages/onlineMap/pages/tdt/tdt.component.ts
@@ -24,18 +24,22 @@ export class TdtComponent {
 		};
 		this.servers = [
 			{label: '--选择服务类型--', value: null},
-			{label: '天地图全球影像地图服务（经纬度投影）', value: new TiandituImageryProvider(TiandituMapsStyle.IMG_C)},
-			{label: '天地图全球影像地图服务（墨卡托投影）', value: new TiandituImageryProvider(TiandituMapsStyle.IMG_W)},
-			{label: '天地图全球地形晕渲服务（经纬度投影）', value: new TiandituImageryProvider(TiandituMapsStyle.TER_C)},
-			{label: '天地图全球地形晕渲服务（墨卡托投影）', value: new TiandituImageryProvider(TiandituMapsStyle.TER_W)},
-			{label: '天地图全球矢量地图服务（经纬度投影）', value: new TiandituImageryProvider(TiandituMapsStyle.VEC_C)},
-			{label: '天地图全球矢量地图服务（墨卡托投影）', value: new TiandituImageryProvider(TiandituMapsStyle.VEC_W)},
-			{label: '天地图全球影像中文注记服务（经纬度投影）', value: new TiandituImageryProvider(TiandituMapsStyle.CIA_C)},
-			{label: '天地图全球影像中文注记服务（墨卡托投影）', value: new TiandituImageryProvider(TiandituMapsStyle.CIA_W)},
-			{label: '天地图全球地形中文注记服务（经纬度投影）', value: new TiandituImageryProvider(TiandituMapsStyle.CTA_C)},
-			{label: '天地图全球地形中文注记服务（墨卡托投影）', value: new TiandituImageryProvider(TiandituMapsStyle.CTA_W)},
-			{label: '天地图全球矢量中文标注服务（经纬度投影）', value: new TiandituImageryProvider(TiandituMapsStyle.CVA_C)},
-			{label: '天地图全球矢量中文标注服务（墨卡托投影）', value: new TiandituImageryProvider(TiandituMapsStyle.CVA_W)}
+			{label: '全球影像地图服务（经纬度投影）', value: new TiandituImageryProvider(TiandituMapsStyle.IMG_C)},
+			{label: '全球影像地图服务（墨卡托投影）', value: new TiandituImageryProvider(TiandituMapsStyle.IMG_W)},
+			{label: '全球地形晕渲服务（经纬度投影）', value: new TiandituImageryProvider(TiandituMapsStyle.TER_C)},
+			{label: '全球地形晕渲服务（墨卡托投影）', value: new TiandituImageryProvider(TiandituMapsStyle.TER_W)},
+			{label: '全球矢量地图服务（经纬度投影）', value: new TiandituImageryProvider(TiandituMapsStyle.VEC_C)},
+			{label: '全球矢量地图服务（墨卡托投影）', value: new TiandituImageryProvider(TiandituMapsStyle.VEC_W)},
+			{label: '全球影像中文注记服务（经纬度投影）', value: new TiandituImageryProvider(TiandituMapsStyle.CIA_C)},
+			{label: '全球影像中文注记服务（墨卡托投影）', value: new TiandituImageryProvider(TiandituMapsStyle.CIA_W)},
+			{label: '全球影像英文注记服务（经纬度投影）', value: new TiandituImageryProvider(TiandituMapsStyle.EIA_C)},
+			{label: '全球影像英文注记服务（墨卡托投影）', value: new TiandituImageryProvider(TiandituMapsStyle.EIA_W)},
+			{label: '全球地形中文注记服务（经纬度投影）', value: new TiandituImageryProvider(TiandituMapsStyle.CTA_C)},
+			{label: '全球地形中文注记服务（墨卡托投影）', value: new TiandituImageryProvider(TiandituMapsStyle.CTA_W)},
+			{label: '全球矢量中文标注服务（经纬度投影）', value: new TiandituImageryProvider(TiandituMapsStyle.CVA_C)},
+			{label: '全球矢量中文标注服务（墨卡托投影）', value: new TiandituImageryProvider(TiandituMapsStyle.CVA_W)},
+			{label: '全球矢量英文标注服务（经纬度投影）', value: new TiandituImageryProvider(TiandituMapsStyle.EVA_C)},
+			{label: '全球矢量英文标注服务（墨卡托投影）', value: new TiandituImageryProvider(TiandituMapsStyle.EVA_W)}
 		];
 	}
 
