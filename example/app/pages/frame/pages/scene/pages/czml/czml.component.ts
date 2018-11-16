df5324493d1da629582869bae250a03c0dac0ec9
laixiangran <laixiangran@163.com>
2018-11-16 18:34:10 +0800

    修改示例

--- example/app/pages/frame/pages/scene/pages/czml/czml.component.ts
+++ example/app/pages/frame/pages/scene/pages/czml/czml.component.ts
@@ -4,6 +4,7 @@ import Viewer = Cesium.Viewer;
 import Scene = Cesium.Scene;
 import Globe = Cesium.Globe;
 import CzmlDataSource = Cesium.CzmlDataSource;
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
 	templateUrl: './czml.component.html',
@@ -14,8 +15,20 @@ export class CZMLComponent implements OnInit {
 	viewer: Viewer;
 	scene: Scene;
 	globe: Globe;
+	coords: any[] = [];
+	layerColor: any = {
+		1: [169, 169, 0, 255],
+		2: [84, 255, 126, 255],
+		3: [255, 255, 0, 255],
+		4: [0, 255, 126, 255],
+		5: [255, 255, 126, 255],
+		6: [0, 255, 0, 255]
+	};
 
-	constructor() {
+	constructor(public http: HttpClient) {
+		this.viewerOptions = {
+			globe: false
+		};
 	}
 
 	ngOnInit() {
@@ -25,74 +38,75 @@ export class CZMLComponent implements OnInit {
 		this.viewer = evt.viewer;
 		this.scene = evt.scene;
 		this.globe = evt.globe;
-		this.addCZML();
+		this.getCZMLData().then((data: any) => {
+			this.coords = data;
+			this.addCZML(data);
+		});
 	}
 
-	addCZML() {
-		const czml = [{
+	getCZMLData(): Promise<any> {
+		return new Promise<any>((resolve, reject) => {
+			const headers = new HttpHeaders({
+					'Content-Type': 'application/json'
+				}),
+				options = {headers: headers};
+			this.http.get('./assets/json/layer_1.json', options).subscribe((data: any) => {
+				const features: any = data.features;
+				const coords: any[] = [];
+				features.forEach((feature) => {
+					const rings: number[] = [];
+					feature.geometry.rings[0].forEach((ring, index) => {
+						if (index < 3) {
+							ring[2] = ring[2] * 5;
+							[].push.apply(rings, ring);
+						}
+					});
+					coords.push(rings);
+				});
+				resolve(coords);
+			}, (error: any) => {
+				reject(error);
+			});
+		});
+	}
+
+	addCZML(coords: any[], top: boolean = false) {
+		const polygonCZML: any[] = [{
 			'id': 'document',
-			'name': 'box',
+			'name': 'CZML Geometries: Polygon',
 			'version': '1.0'
-		}, {
-			'id': 'shape1',
-			'name': 'Blue box',
-			'position': {
-				'cartographicDegrees': [-114.0, 40.0, 300000.0]
-			},
-			'box': {
-				'dimensions': {
-					'cartesian': [400000.0, 300000.0, 500000.0]
-				},
-				'material': {
-					'solidColor': {
-						'color': {
-							'rgba': [0, 0, 255, 255]
-						}
-					}
-				}
-			}
-		}, {
-			'id': 'shape2',
-			'name': 'Red box with black outline',
-			'position': {
-				'cartographicDegrees': [-107.0, 40.0, 300000.0]
-			},
-			'box': {
-				'dimensions': {
-					'cartesian': [400000.0, 300000.0, 500000.0]
-				},
-				'material': {
-					'solidColor': {
-						'color': {
-							'rgba': [255, 0, 0, 128]
-						}
-					}
-				},
-				'outline': true,
-				'outlineColor': {
-					'rgba': [0, 0, 0, 255]
-				}
-			}
-		}, {
-			'id': 'shape3',
-			'name': 'Yellow box outline',
-			'position': {
-				'cartographicDegrees': [-100.0, 40.0, 300000.0]
-			},
-			'box': {
-				'dimensions': {
-					'cartesian': [400000.0, 300000.0, 500000.0]
-				},
-				'fill': false,
-				'outline': true,
-				'outlineColor': {
-					'rgba': [255, 255, 0, 255]
-				}
-			}
 		}];
-		CzmlDataSource.load(czml).then((czmlDataSource: CzmlDataSource) => {
-			this.viewer.dataSources.add(czmlDataSource);
-			this.viewer.zoomTo(czmlDataSource);
+		coords.forEach((coord, index) => {
+			const py = {
+				'id': top ? 'top' + index : 'bottom' + index,
+				'name': top ? 'top' + index : 'bottom' + index,
+				'polygon': {
+					'positions': {
+						'cartographicDegrees': coord
+					},
+					'material': {
+						'solidColor': {
+							'color': {
+								'rgba': this.layerColor['1']
+							}
+						}
+					},
+					'extrudedHeight': 0,
+					'perPositionHeight': true,
+					'outline': true,
+					'outlineColor': {
+						'rgba': [255, 255, 255, 50]
+					}
+				}
+			};
+			polygonCZML.push(py);
+		});
+		const start = new Date().getTime();
+		const dataSourcePromise: Promise<CzmlDataSource> = CzmlDataSource.load(polygonCZML);
+		this.viewer.dataSources.add(dataSourcePromise);
+		this.viewer.zoomTo(dataSourcePromise).then(() => {
+			const end = new Date().getTime();
+			console.log(end - start);
 		});
 	}
 }
